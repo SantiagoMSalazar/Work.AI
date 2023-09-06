553bca56ca882a7e9c72b24e61fd72840d89a6f4
Santiago Miguel Salazar <santiago.salazar@epn.edu.ec>
2023-09-05 20:55:40 -0500

    fix: reparing the model build: applying normalization input data

--- backend/app.py
+++ backend/app.py
@@ -6,29 +6,23 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app)
 
-# Carga el modelo .pkl
-modelo = joblib.load('./trained model/linealM.pkl')
+# modelos entrenados
+modelo = joblib.load('./models/LinealRegresion.pkl')
+scaler = joblib.load('./models/Scaler.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predecir():
     try:
-        # Recibe los datos del JSON de entrada
         datos_entrada = flask.request.json
-
-        # Asegúrate de que se proporcionen las 5 variables
         if len(datos_entrada) != 5:
             return "Se requieren 5 variables de entrada", 400
-
-        # Convierte los datos en un formato adecuado para el modelo
         datos_entrada = [datos_entrada['tipo_establecimiento'], datos_entrada['provincia_desc'], datos_entrada['ciiu_desc'], datos_entrada['personal_ocupado'], datos_entrada['ventas']]
 
+        # normalizar los datos con el scaler entrenado
+        datos_norm= scaler.transform([datos_entrada])
         # Realiza la predicción utilizando el modelo
-        resultado = modelo.predict([datos_entrada])[0]
-
-        # Crea una respuesta JSON con el resultado
+        resultado = modelo.predict(datos_norm)[0]
         respuesta = {'resultado': resultado}
-
-        # Devuelve la respuesta en formato JSON
         return flask.jsonify(respuesta)
 
     except Exception as e:
